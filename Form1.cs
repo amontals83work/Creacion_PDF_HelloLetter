0c21ecbc7977ecae445f7bdf964efa6926e75ade
Alejandro Montals Pozzoli <amontals83.work@gmail.com>
2023-11-24 13:33:34 +0100

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Creacion_PDF_HelloLetter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CenterToScreen();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
