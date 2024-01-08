b9fdc072ec7a1d4b9e96126c525e96c2a2ccf46f
Alejandro Montals Pozzoli <amontals83.work@gmail.com>
2024-01-08 13:24:21 +0100

    PAGANTIS a parte

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,11 +36,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(19, 24);
+            this.label1.Location = new System.Drawing.Point(57, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(162, 17);
+            this.label1.Size = new System.Drawing.Size(80, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "CARTERA PAGANTIS";
+            this.label1.Text = "CARTERA";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button1
@@ -62,7 +62,6 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
